ac879af77c9d07ed71207018055c34c8bfddce35
Sreeja Panyala <sreejapanyala24@gmail.com>
2026-04-24 18:04:17 -0500

    add age to greet func

--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import "fmt"
 
-func greet(name string) {
+func greet(name string, age int) {
 	fmt.Println("Hello,", name)
 }
 
 func main() {
 	var name string
+	var age int
 	fmt.Println("Enter your name?")
 	fmt.Scanln(&name)
-	greet(name)
+
+	fmt.Println("Enter your age?")
+	fmt.Scanln(&age)
+
+	greet(name, age)
 }
